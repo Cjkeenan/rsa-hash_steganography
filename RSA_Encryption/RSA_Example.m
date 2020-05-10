4cf932d4fafee496337e6bca3b2cd24eae443c58
Cjkeenan <cjkeenan@live.com>
2020-05-10 00:02:53 -0700

    Update RSA_Example.m
    Reverted to the original example

--- RSA_Encryption/RSA_Example.m
+++ RSA_Encryption/RSA_Example.m
@@ -23,11 +23,7 @@
 %     print("Decrypted Decoded Text: {0}" .format(textDecryptedPT))
 
 clear; close all;
-%image to be read
-I = imread('image.jpg');
-% % N = 259
-% p = 7;
-% q = 37;
+
 p = 215417;
 q = 124513;
 bits = 64;
@@ -36,22 +32,5 @@ bits = 64;
 text = "To be, or not to be- that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them. To die- to sleep- No more; and by a sleep to say we end The heartache, and the thousand natural shocks That flesh is heir to. 'Tis a consummation Devoutly to be wish'd. To die- to sleep. To sleep- perchance to dream: ay, there's the rub!";
 textEncoded = double(char(text));
 textEncrypted = encryptRSA(textEncoded,e,n);
-%convert to uint64 to save char space
-textEncryptedL = uint64(textEncrypted);
-[a,b] = size(textEncryptedL);
-%null string to store everything
-totalStr = ""; 
-for i = 1:1:b
-    totalStr =strcat(totalStr,num2str(textEncrypted(i))," ");
-end
-sM = char(totalStr);
-[Ix, sizeX] = Picencode(I,sM);
-textEncChar = Picdecode(Ix,sizeX);
-textEncStr = convertCharsToStrings(textEncChar);
-textEncSplit = split(textEncStr);
-[c,e] = size(textEncSplit);
-textEnc = str2double(textEncSplit);
-textEnc(436) = [];
-textEnc = textEnc.';
-textDecrypted = decryptRSA(textEnc,d,n);
+textDecrypted = decryptRSA(textEncrypted,d,n);
 textDecoded = string(char(textDecrypted));
