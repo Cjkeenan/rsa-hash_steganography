131b688a79feb158cd823bf11bcc55c4f99b339d
Dmheer <57122207+Dmheer@users.noreply.github.com>
2020-05-10 00:27:19 -0700

    Added the Pathfinding

--- RSA_Hash_example.m
+++ RSA_Hash_example.m
@@ -1,9 +1,10 @@
 clear; close all;
-
+addpath('RSA-Hash_Steganography');
+addpath('HashLSB_Steganography');
+addpath('RSA_Encryption');
 p = 215417;
 q = 124513;
 bits = 64;
-
 image = imread('image.jpg');
 text = "To be, or not to be- that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them. To die- to sleep- No more; and by a sleep to say we end The heartache, and the thousand natural shocks That flesh is heir to. 'Tis a consummation Devoutly to be wish'd. To die- to sleep. To sleep- perchance to dream: ay, there's the rub!";
 [e,d,n] = generateRSAkeys(p,q,bits);
